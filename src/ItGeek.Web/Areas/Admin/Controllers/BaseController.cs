ea9e9638a47c0c30e98e193d4a0b64e120482ba3
aradikdev <aradikdev@gmail.com>
2023-05-17 19:54:32 +0600

    BaseController

---
+++ src/ItGeek.Web/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,70 @@
+﻿using ItGeek.BLL;
+using ItGeek.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItGeek.Web.Areas.Admin.Controllers;
+
+public abstract class BaseController : Controller
+{
+	private readonly UnitOfWork _uow;
+
+	public BaseController(UnitOfWork uow)
+    {
+		_uow = uow;
+	}
+    public async Task<IActionResult> Index()
+	{
+		return View(await _uow.TagRepository.ListAllAsync());
+	}
+
+	public async Task<IActionResult> Details(int id)
+	{
+		return View(await _uow.TagRepository.GetByIDAsync(id));
+	}
+
+	public async Task<IActionResult> Delete(int id)
+	{
+		Tag tag = await _uow.TagRepository.GetByIDAsync(id);
+		if (tag != null)
+		{
+			await _uow.TagRepository.DeleteAsync(tag);
+		}
+		return RedirectToAction(nameof(Index));
+	}
+	[HttpGet]
+	public async Task<IActionResult> Create()
+	{
+		return View();
+	}
+	[HttpPost]
+	public async Task<IActionResult> Create(Tag tag)
+	{
+		if (ModelState.IsValid)
+		{
+			await _uow.TagRepository.InsertAsync(tag);
+			return RedirectToAction(nameof(Index));
+		}
+		return View(tag);
+	}
+	[HttpGet]
+	public async Task<IActionResult> Update(int id)
+	{
+		Tag tag = await _uow.TagRepository.GetByIDAsync(id);
+		if (tag == null)
+		{
+			return NotFound();
+		}
+		return View(tag);
+	}
+	[HttpPost]
+	public async Task<IActionResult> Update(Tag tag)
+	{
+		if (ModelState.IsValid)
+		{
+			await _uow.TagRepository.UpdateAsync(tag);
+			return RedirectToAction(nameof(Index));
+		}
+		return View(tag);
+	}
+
+}
