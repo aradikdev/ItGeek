2c6ee45e542cac56650af28317e094015ebed730
aradikdev <aradikdev@gmail.com>
2023-05-31 19:21:01 +0600

    fix User and Role relations

--- src/ItGeek.DAL/Entities/User.cs
+++ src/ItGeek.DAL/Entities/User.cs
@@ -7,5 +7,5 @@ public class User : BaseEntity
     public string Email { get; set; }
     public string Password { get; set; }
     public int RoleId { get; set; }
-    public Role Role { get; set; }
+    public virtual Role? Role { get; set; }
 }
